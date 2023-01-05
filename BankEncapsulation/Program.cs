3c29b3c8dd334629e843ec9d775ee07111622cd1
Spencer Hansen <spencerhansen@gmail.com>
2023-01-04 17:08:32 -0700

    finished this exercise on encapsulation

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            Console.WriteLine("how many dollars would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+            Console.WriteLine($"thank you! youre balance is now ${account.GetBalance()}");
         }
     }
 }
